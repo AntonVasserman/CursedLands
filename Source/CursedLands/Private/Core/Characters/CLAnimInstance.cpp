b438d004f050b6164bf5f093d3aef9fb167b9691
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-03-10 23:04:34 +0200

    Refactor(CL-4): Added CLAnimInstance Class

---
+++ Source/CursedLands/Private/Core/Characters/CLAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "Core/Characters/CLAnimInstance.h"
+
+#include "Core/Characters/CLCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UCLAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (ACLCharacter* OwningCharacter = Cast<ACLCharacter>(GetOwningActor()))
+	{
+		Character = OwningCharacter;
+		MovementComponent = Character->GetCharacterMovement();
+	}
+}
+
+void UCLAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Character)
+	{
+		Velocity = MovementComponent->Velocity;
+		GroundSpeed = UKismetMathLibrary::VSizeXY(Velocity);
+		bShouldMove = GroundSpeed > 3.0f && MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector;
+		bIsFalling = MovementComponent->IsFalling();
+	}
+}
---
+++ Source/CursedLands/Public/Core/Characters/CLAnimInstance.h
@@ -0,0 +1,41 @@
+// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CLAnimInstance.generated.h"
+
+class ACLCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class CURSEDLANDS_API UCLAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	//~ UAnimInstance Begin
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	//~ UAnimInstance End
+	
+private:
+	UPROPERTY(BlueprintReadOnly, Category = "References", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<ACLCharacter> Character;
+
+	UPROPERTY(BlueprintReadOnly, Category = "References", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCharacterMovementComponent> MovementComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Essential Movement Data", meta = (AllowPrivateAccess = "true"))
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Essential Movement Data", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Essential Movement Data", meta = (AllowPrivateAccess = "true"))
+	bool bShouldMove;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Essential Movement Data", meta = (AllowPrivateAccess = "true"))
+	bool bIsFalling;
+};
