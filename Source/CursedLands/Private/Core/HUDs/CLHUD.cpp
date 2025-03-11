39af2fd8dbcf6d84d0f150c0a2836210e6c00bac
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-03-11 07:32:15 +0200

    Refactor(CL-6): Added HUD Class and Blueprint

---
+++ Source/CursedLands/Private/Core/HUDs/CLHUD.cpp
@@ -0,0 +1,5 @@
+// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "Core/HUDs/CLHUD.h"
+
---
+++ Source/CursedLands/Public/Core/HUDs/CLHUD.h
@@ -0,0 +1,14 @@
+// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CLHUD.generated.h"
+
+UCLASS()
+class CURSEDLANDS_API ACLHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
