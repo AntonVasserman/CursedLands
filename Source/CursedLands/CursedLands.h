2d6e7f53f1e400278779352791fc15573fed288e
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-05-02 15:35:07 +0300

    CL-na (Polish): Added Curves for FootSpeed and added them to the Foot Placement Animation Node

--- Source/CursedLands/CursedLands.h
+++ Source/CursedLands/CursedLands.h
@@ -8,7 +8,4 @@
 // TODO: Check if there is a better solution than ANS_Exit
 // TODO: Change Traverse Input after research
 // TODO: Traverse while sprinting should be automatic?
-// TODO (CL-73): Cut Sprint Start and Stop animations
 // TODO (CL-74): Check that Start has Striding... (Check what Lyra did)
-// TODO (CL-75): Cut Walk Stop animations
-// TODO (CL-76): Use Sprint Lean Poses
