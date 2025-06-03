581f3ae0cae451997a58b63ce26df60941d4ca92
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-06-04 00:08:34 +0300

    CL-na (Refactor): Updated AVPlugins and improved teleporting in Parkour Gym

--- Source/CursedLands.Target.cs
+++ Source/CursedLands.Target.cs
@@ -9,7 +9,7 @@ public class CursedLandsTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("CursedLands");
 	}
 }
