bb0e2bf58084d7067c070037dce19efee36c0976
AntonVasserman <anton.vasserman@outlook.com>
2025-07-27 21:04:35 +0300

    CL-185 (Bug|WIP): GCS using StateTrees

---
+++ Source/CursedLands/Private/Characters/Gameplay/Camera/CL_GetPlayerCharacterCameraInfoGCSTTask.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "Characters/Gameplay/Camera/CL_GetPlayerCharacterCameraInfoGCSTTask.h"
+
+#include "CL_LogChannels.h"
+#include "StateTreeExecutionContext.h"
+#include "Characters/CL_PlayerCharacter.h"
+#include "GameFramework/GameplayCameraComponent.h"
+
+//~ FGameplayCamerasStateTreeTask Begin
+
+EStateTreeRunStatus FCL_GetPlayerCharacterCameraInfoGCSTTask::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	FGameplayCamerasStateTreeTask::EnterState(Context, Transition);
+
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	InstanceData.PlayerCharacterRef = Cast<ACL_PlayerCharacter>(Cast<UGameplayCameraComponent>(Context.GetOwner())->GetOwner());
+	if (!InstanceData.PlayerCharacterRef)
+	{
+		UE_LOG(LogCL, Error, TEXT("Failed to get PlayerCharacterRef from StateTreeExecutionContext"));
+		return EStateTreeRunStatus::Failed;
+	}
+	
+	return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FCL_GetPlayerCharacterCameraInfoGCSTTask::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	FGameplayCamerasStateTreeTask::Tick(Context, DeltaTime);
+
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (!InstanceData.PlayerCharacterRef)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+
+	InstanceData.Stance = InstanceData.PlayerCharacterRef->GetCLCharacterMovement()->GetStance();
+	InstanceData.CameraMode = InstanceData.PlayerCharacterRef->GetCameraMode();
+	InstanceData.bSprinting = InstanceData.PlayerCharacterRef->IsSprinting();
+	
+	return EStateTreeRunStatus::Running;
+}
+
+//~ FGameplayCamerasStateTreeTask End
---
+++ Source/CursedLands/Public/Characters/Gameplay/Camera/CL_GetPlayerCharacterCameraInfoGCSTTask.h
@@ -0,0 +1,48 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Directors/CameraDirectorStateTreeSchema.h"
+#include "UObject/Object.h"
+
+#include "CL_GetPlayerCharacterCameraInfoGCSTTask.generated.h"
+
+enum class ECL_PlayerCharacterCameraMode : uint8;
+enum class ECL_Stance : uint8;
+class ACL_PlayerCharacter;
+
+USTRUCT()
+struct CURSEDLANDS_API FCL_GetPlayerCharacterCameraInfoGCSTTaskInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	ACL_PlayerCharacter* PlayerCharacterRef = nullptr;
+	
+	UPROPERTY(EditAnywhere, Category = "Outputs")
+	ECL_Stance Stance;
+
+	UPROPERTY(EditAnywhere, Category = "Outputs")
+	ECL_PlayerCharacterCameraMode CameraMode;
+
+	UPROPERTY(EditAnywhere, Category = "Outputs")
+	bool bSprinting;
+};
+
+USTRUCT(Meta = (DisplayName = "Get Player Character Camera Info", Category = "Cameras"))
+struct CURSEDLANDS_API FCL_GetPlayerCharacterCameraInfoGCSTTask : public FGameplayCamerasStateTreeTask
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FCL_GetPlayerCharacterCameraInfoGCSTTaskInstanceData;
+
+	FCL_GetPlayerCharacterCameraInfoGCSTTask() = default;
+
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+	
+	//~ FGameplayCamerasStateTreeTask Begin
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	//~ FGameplayCamerasStateTreeTask End
+};
