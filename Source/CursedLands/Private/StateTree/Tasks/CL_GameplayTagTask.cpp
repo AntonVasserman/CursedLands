db540e259e8202468280b719e7d9d8215a6a6b44
AntonVasserman <anton.vasserman@outlook.com>
2025-07-31 23:24:27 +0300

    CL-201 (Task|WIP): Migrate CFC to StateTrees

---
+++ Source/CursedLands/Private/StateTree/Tasks/CL_GameplayTagTask.cpp
@@ -0,0 +1,58 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "StateTree/Tasks/CL_GameplayTagTask.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "CL_LogChannels.h"
+#include "StateTreeExecutionContext.h"
+#include "Controllers/CL_PlayerController.h"
+
+FCL_GameplayTagTask::FCL_GameplayTagTask()
+{
+	bShouldCallTick = false;
+
+#if WITH_EDITORONLY_DATA
+	bConsideredForCompletion = false;
+	bCanEditConsideredForCompletion = false;
+#endif
+}
+
+EStateTreeRunStatus FCL_GameplayTagTask::EnterState(
+	FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	AActor* Actor = InstanceData.Controller->GetPawn();
+	UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Actor);
+
+	// If we can't find the Component we won't throw, we will just not add the Tag
+	if (AbilitySystemComponent == nullptr)
+	{
+		UE_LOG(LogCL, Warning, TEXT("%s::%hs: Couldn't find AbilitySystemComponent on for Actor: %s"), *StaticStruct()->GetName(), __FUNCTION__, *Actor->GetFullName());
+		return EStateTreeRunStatus::Failed;
+	}
+
+	AbilitySystemComponent->SetLooseGameplayTagCount(InstanceData.GameplayTag, 1);
+	
+	return EStateTreeRunStatus::Running;
+}
+
+void FCL_GameplayTagTask::ExitState(FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	AActor* Actor = InstanceData.Controller->GetPawn();
+	UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Actor);
+
+	// If we can't find the Component we won't throw, we will just not add the Tag
+	if (AbilitySystemComponent == nullptr)
+	{
+		UE_LOG(LogCL, Warning, TEXT("%s::%hs: Couldn't find AbilitySystemComponent on for Actor: %s"), *StaticStruct()->GetName(), __FUNCTION__, *Actor->GetFullName());
+	}
+	
+	AbilitySystemComponent->RemoveLooseGameplayTag(InstanceData.GameplayTag);
+}
---
+++ Source/CursedLands/Public/StateTree/Tasks/CL_GameplayTagTask.h
@@ -0,0 +1,37 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "UObject/Object.h"
+#include "CL_GameplayTagTask.generated.h"
+
+class UCL_GASOperationBase;
+class ACL_PlayerController;
+
+USTRUCT()
+struct CURSEDLANDS_API FCL_GameplayTagTaskInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Context")
+	TObjectPtr<ACL_PlayerController> Controller;
+
+	UPROPERTY(EditAnywhere, Category = "", Meta = (AllowPrivateAccess = "true"))
+	FGameplayTag GameplayTag;
+};
+
+USTRUCT(Meta = (DisplayName = "Gameplay Tag StateTree Task"))
+struct CURSEDLANDS_API FCL_GameplayTagTask : public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+	FCL_GameplayTagTask();
+	
+	using FInstanceDataType = FCL_GameplayTagTaskInstanceData;
+
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+};
