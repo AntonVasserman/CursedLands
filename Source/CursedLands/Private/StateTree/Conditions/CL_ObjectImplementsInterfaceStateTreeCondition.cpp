1a421662710b732ed8a6627f61a373d35b5f7126
AntonVasserman <anton.vasserman@outlook.com>
2025-08-01 17:46:48 +0300

    CL-201 (Task|WIP): Migrate CFC to StateTrees

---
+++ Source/CursedLands/Private/StateTree/Conditions/CL_ObjectImplementsInterfaceStateTreeCondition.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "StateTree/Conditions/CL_ObjectImplementsInterfaceStateTreeCondition.h"
+
+#include "StateTreeExecutionContext.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+bool FCL_ObjectImplementsInterfaceStateTreeCondition::TestCondition(FStateTreeExecutionContext& Context) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (!InstanceData.Interface)
+	{
+		return false;
+	}
+
+	const bool bImplementsInterface = UKismetSystemLibrary::DoesImplementInterface(InstanceData.Object, InstanceData.Interface);
+	return bImplementsInterface ^ InstanceData.bInvert;
+}
+
+#if WITH_EDITOR
+FText FCL_ObjectImplementsInterfaceStateTreeCondition::GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView, const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting) const
+{
+	const FInstanceDataType* InstanceData = InstanceDataView.GetPtr<FInstanceDataType>();
+	
+	if (!InstanceData)
+	{
+		return FText::FromString(TEXT("Object Implements Interface"));
+	}
+
+	FString InterfaceName = TEXT("None");
+	if (InstanceData->Interface)
+	{
+		InterfaceName = InstanceData->Interface->GetName();
+	}
+
+	return FText::FromString(FString::Printf(TEXT("Object%s Implements %s"), InstanceData->bInvert ? TEXT(" not") : TEXT(""), *InterfaceName));
+}
+#endif
---
+++ Source/CursedLands/Public/StateTree/Conditions/CL_ObjectImplementsInterfaceStateTreeCondition.h
@@ -0,0 +1,38 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeConditionBase.h"
+#include "UObject/Object.h"
+#include "CL_ObjectImplementsInterfaceStateTreeCondition.generated.h"
+
+USTRUCT()
+struct CURSEDLANDS_API FCL_ObjectImplementsInterfaceStateTreeConditionInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Inputs")
+	TObjectPtr<UObject> Object;
+
+	UPROPERTY(EditAnywhere, Category = "Parameter", Meta = (AllowAbstract = "true"))
+	TSubclassOf<UInterface> Interface;
+
+	UPROPERTY(EditAnywhere, Category = "Parameter")
+	bool bInvert = false;
+};
+
+USTRUCT(DisplayName = "Object Implements Interface")
+struct CURSEDLANDS_API FCL_ObjectImplementsInterfaceStateTreeCondition : public FStateTreeConditionCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FCL_ObjectImplementsInterfaceStateTreeConditionInstanceData;
+	
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+	virtual bool TestCondition(FStateTreeExecutionContext& Context) const override;
+	
+#if WITH_EDITOR
+	virtual FText GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView, const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting = EStateTreeNodeFormatting::Text) const override;
+#endif
+};
