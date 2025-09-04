1fe6932bae1c038b299ab54e19b8e8f32460cb36
AntonVasserman <anton.vasserman@outlook.com>
2025-09-04 12:26:36 +0300

    235 (Task|WIP): Added Custom Tab List with Buttons

---
+++ Source/CursedLands/Private/UI/Settings/CL_CommonTabListWithButtonsWidgetBase.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+
+#include "UI/Settings/CL_CommonTabListWithButtonsWidgetBase.h"
+
+#include "CommonButtonBase.h"
+
+void UCL_CommonTabListWithButtonsWidgetBase::PreviousTabButton_OnClicked()
+{
+	bool bPassThrough;
+	HandlePreviousTabInputAction(bPassThrough);
+}
+
+void UCL_CommonTabListWithButtonsWidgetBase::NextTabButton_OnClicked()
+{
+	bool bPassThrough;
+	HandleNextTabInputAction(bPassThrough);
+}
+
+//~ Begin UCommonTabListWidgetBase
+
+void UCL_CommonTabListWithButtonsWidgetBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	PreviousTabButton->SetTriggeredInputAction(PreviousTabInputActionData);
+	NextTabButton->SetTriggeredInputAction(NextTabInputActionData);
+	
+	// Setup Bindings
+	PreviousTabButtonOnClickedHandle = PreviousTabButton->OnClicked().AddUObject(this, &UCL_CommonTabListWithButtonsWidgetBase::PreviousTabButton_OnClicked);
+	NextTabButtonOnClickedHandle = NextTabButton->OnClicked().AddUObject(this, &UCL_CommonTabListWithButtonsWidgetBase::NextTabButton_OnClicked);
+}
+
+void UCL_CommonTabListWithButtonsWidgetBase::NativeDestruct()
+{
+	Super::NativeDestruct();
+
+	// Clear Bindings
+	PreviousTabButton->OnClicked().Remove(PreviousTabButtonOnClickedHandle);
+	NextTabButton->OnClicked().Remove(NextTabButtonOnClickedHandle);
+}
+
+//~ End UCommonTabListWidgetBase
---
+++ Source/CursedLands/Public/UI/Settings/CL_CommonTabListWithButtonsWidgetBase.h
@@ -0,0 +1,34 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "Blueprint/UserWidget.h"
+#include "CL_CommonTabListWithButtonsWidgetBase.generated.h"
+
+UCLASS(Abstract, Blueprintable, ClassGroup = UI, Meta = (DisableNativeTick))
+class CURSEDLANDS_API UCL_CommonTabListWithButtonsWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(BlueprintReadOnly, Meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	TObjectPtr<UCommonButtonBase> PreviousTabButton;
+	FDelegateHandle PreviousTabButtonOnClickedHandle;
+
+	UPROPERTY(BlueprintReadOnly, Meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	TObjectPtr<UCommonButtonBase> NextTabButton;
+	FDelegateHandle NextTabButtonOnClickedHandle;
+
+	UFUNCTION()
+	void PreviousTabButton_OnClicked();
+	UFUNCTION()
+	void NextTabButton_OnClicked();
+	
+	//~ Begin UCommonTabListWidgetBase
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	//~ End UCommonTabListWidgetBase
+};
