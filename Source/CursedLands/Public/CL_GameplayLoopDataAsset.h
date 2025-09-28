03f34afef827e577e521032dee70b6d9fb39892b
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-09-28 17:53:18 +0300

    302 (Task): Gameplay Loop POC

---
+++ Source/CursedLands/Public/CL_GameplayLoopDataAsset.h
@@ -0,0 +1,22 @@
+﻿// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CL_GameplayLoopDataAsset.generated.h"
+
+UCLASS(BlueprintType, Const, Meta = (DisplayName = "Gameplay Loop Data"))
+class CURSEDLANDS_API UCL_GameplayLoopDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 WaveCount = 1;
+
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override
+	{
+		return FPrimaryAssetId("GameplayLoopData", "Default");
+	}
+};
